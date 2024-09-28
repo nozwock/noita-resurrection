99b7cbec653c0dc1b1db5aafe2ac3e6a3c444233
nozwock <57829219+nozwock@users.noreply.github.com>
2024-09-28 15:19:02 +0530

    Impl a dialog box for respawn choice

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+dofile("data/scripts/lib/utilities.lua")
+dofile("data/scripts/debug/keycodes.lua")
+
 local ONE_HP = 0.04
 
 local respawn_position = {
@@ -8,7 +11,101 @@ local respawn_position = {
 local respawn_messages = { "But you're better now.", "Let's get you fixed up.", "Take a deep breath.",
 	"Oooh! What in blazes...?" }
 
+local function IdFactory()
+	local id_ = 0
+
+	return function()
+		id_ = id_ + 1
+		return id_
+	end
+end
+
+local function centered_x(gui, object_width)
+	local w, _ = GuiGetScreenDimensions(gui)
+	return math.floor((w / 2 - object_width / 2) / w * 100)
+end
+
+local function GuiDecoratedText(gui, id, y, title)
+	title = string.upper(title)
+
+	local edge_w, edge_h = GuiGetImageDimensions(gui, "data/ui_gfx/decorations/piece_small_left.png", 1)
+	local middle_w, middle_h = GuiGetImageDimensions(gui, "data/ui_gfx/decorations/piece_small_middle.png", 1)
+	local text_w, _ = GuiGetTextDimensions(gui, title, 2)
+
+	local padding_scale = 6
+	local middle_scale = math.ceil(text_w / middle_w)
+	local decor_w = edge_w * 2 + middle_w * (middle_scale + padding_scale * 2)
+
+	local w, h = GuiGetScreenDimensions(gui)
+	local decor_x_scale = math.floor((w / 2 - decor_w / 2) / w * 100)
+	local text_x_scale = math.floor((w / 2 - text_w / 2) / w * 100)
+
+	GuiLayoutBeginHorizontal(gui, decor_x_scale, y, false, 0, 0)
+	GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_left.png", 1, 1, 0, 0,
+		GUI_RECT_ANIMATION_PLAYBACK.Loop)
+	GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_middle.png", 1, middle_scale + padding_scale * 2,
+		1, 0, GUI_RECT_ANIMATION_PLAYBACK.Loop)
+	GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_right.png",
+		1, 1, 0, 0,
+		GUI_RECT_ANIMATION_PLAYBACK.Loop)
+	GuiLayoutEnd(gui)
+
+	GuiLayoutBeginVertical(gui, text_x_scale, y)
+	GuiText(gui, 0, 2, title, 2)
+	GuiLayoutEnd(gui)
+end
+
+
+local gui = GuiCreate()
+local draw_ui = false
+
+local function CreateRespawnGui(gui, on_ok, on_cancel)
+	return function()
+		if not draw_ui then
+			return
+		end
+
+		local newid = IdFactory()
+		GuiStartFrame(gui)
+
+		local title_text = string.upper("Unthethered")
+		GuiDecoratedText(gui, newid(), 70, title_text)
+
+		local ok_text = "Wake Up"
+		local ok_text_w, _ = GuiGetTextDimensions(gui, ok_text)
+		local cancel_text = "Wake Up"
+		local cancel_text_w, _ = GuiGetTextDimensions(gui, cancel_text)
+
+		local x_scale = centered_x(gui, ok_text_w + cancel_text_w + 14 + 2)
+		GuiLayoutBeginHorizontal(gui, x_scale, 80)
+		if GuiButton(gui, newid(), 0, 0, "Wake Up") then
+			on_ok()
+			-- draw_ui = not draw_ui
+		end
+		if GuiButton(gui, newid(), 14, 0, "Wisp Away") then
+			on_cancel()
+			-- draw_ui = not draw_ui
+		end
+		GuiLayoutEnd(gui)
+	end
+end
+
+local respawn_ui_update = CreateRespawnGui(gui, function()
+	print("a-ok")
+end, function()
+	print("no revives for you")
+end)
+
 function OnWorldPreUpdate()
+	if InputIsKeyJustUp(Key_k) then
+		GamePrintImportant("Title blablablablablablablabla", "Bruh")
+	end
+	if InputIsKeyJustUp(Key_p) then
+		draw_ui = not draw_ui
+	end
+
+	respawn_ui_update()
+
 	local players = EntityGetWithTag("player_unit")
 	if not (players == nil or #players == 0) then
 		for _, player_id in ipairs(players) do
