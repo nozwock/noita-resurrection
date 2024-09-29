61979d3c67c4c2ee8d15483f5719991860ba2cff
nozwock <57829219+nozwock@users.noreply.github.com>
2024-09-29 18:58:15 +0530

    Convert tabs to spaces

--- init.lua
+++ init.lua
@@ -5,58 +5,58 @@ dofile("data/scripts/status_effects/status_list.lua")
 local ONE_HP = 0.04
 
 local respawn_position = {
-	x = tonumber(MagicNumbersGetValue("DESIGN_PLAYER_START_POS_X")),
-	y = tonumber(MagicNumbersGetValue(
-		"DESIGN_PLAYER_START_POS_Y"))
+  x = tonumber(MagicNumbersGetValue("DESIGN_PLAYER_START_POS_X")),
+  y = tonumber(MagicNumbersGetValue(
+    "DESIGN_PLAYER_START_POS_Y"))
 } -- 227, -85
 
 -- Why am I using different styling for function names? Don't think about it.
 
 local function IdFactory()
-	local id_ = 0
+  local id_ = 0
 
-	return function()
-		id_ = id_ + 1
-		return id_
-	end
+  return function()
+    id_ = id_ + 1
+    return id_
+  end
 end
 
 local function centered_x(gui, object_width)
-	local w, _ = GuiGetScreenDimensions(gui)
-	return math.floor((w / 2 - object_width / 2) / w * 100)
+  local w, _ = GuiGetScreenDimensions(gui)
+  return math.floor((w / 2 - object_width / 2) / w * 100)
 end
 
 local function GuiDecoratedTitle(gui, id, y, title)
-	title = string.upper(title)
+  title = string.upper(title)
 
-	local edge_w, _ = GuiGetImageDimensions(gui, "data/ui_gfx/decorations/piece_small_left.png", 1)
-	local middle_w, _ = GuiGetImageDimensions(gui, "data/ui_gfx/decorations/piece_small_middle.png", 1)
-	local text_w, _ = GuiGetTextDimensions(gui, title, 1, 2, "data/fonts/font_pixel_huge.xml")
+  local edge_w, _ = GuiGetImageDimensions(gui, "data/ui_gfx/decorations/piece_small_left.png", 1)
+  local middle_w, _ = GuiGetImageDimensions(gui, "data/ui_gfx/decorations/piece_small_middle.png", 1)
+  local text_w, _ = GuiGetTextDimensions(gui, title, 1, 2, "data/fonts/font_pixel_huge.xml")
 
-	local padding_scale = 6
-	local middle_scale = text_w / middle_w
-	local decor_w = edge_w * 2 + middle_w * (middle_scale + padding_scale * 2)
+  local padding_scale = 6
+  local middle_scale = text_w / middle_w
+  local decor_w = edge_w * 2 + middle_w * (middle_scale + padding_scale * 2)
 
-	local w, h = GuiGetScreenDimensions(gui)
-	local decor_x = math.floor(w / 2 - decor_w / 2)
-	local text_x = math.floor(w / 2 - text_w / 2)
-	y = h * y / 100
-	-- print(string.format("w:%.2f decor_w:%.2f text_w:%.2f decor_scale:%.2f text_scale:%.2f", w, decor_w, text_w,
-	-- 	decor_x_scale, text_x_scale))
+  local w, h = GuiGetScreenDimensions(gui)
+  local decor_x = math.floor(w / 2 - decor_w / 2)
+  local text_x = math.floor(w / 2 - text_w / 2)
+  y = h * y / 100
+  -- print(string.format("w:%.2f decor_w:%.2f text_w:%.2f decor_scale:%.2f text_scale:%.2f", w, decor_w, text_w,
+  -- 	decor_x_scale, text_x_scale))
 
-	GuiLayoutBeginHorizontal(gui, decor_x, y, true, 0, 0)
-	GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_left.png", 1, 1, 0, 0,
-		GUI_RECT_ANIMATION_PLAYBACK.Loop)
-	GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_middle.png", 1, middle_scale + padding_scale * 2,
-		1, 0, GUI_RECT_ANIMATION_PLAYBACK.Loop)
-	GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_right.png",
-		1, 1, 0, 0,
-		GUI_RECT_ANIMATION_PLAYBACK.Loop)
-	GuiLayoutEnd(gui)
+  GuiLayoutBeginHorizontal(gui, decor_x, y, true, 0, 0)
+  GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_left.png", 1, 1, 0, 0,
+    GUI_RECT_ANIMATION_PLAYBACK.Loop)
+  GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_middle.png", 1, middle_scale + padding_scale * 2,
+    1, 0, GUI_RECT_ANIMATION_PLAYBACK.Loop)
+  GuiImage(gui, id, 0, 0, "data/ui_gfx/decorations/piece_small_right.png",
+    1, 1, 0, 0,
+    GUI_RECT_ANIMATION_PLAYBACK.Loop)
+  GuiLayoutEnd(gui)
 
-	GuiLayoutBeginVertical(gui, text_x, y, true)
-	GuiText(gui, 0, 5, title, 1, "data/fonts/font_pixel_huge.xml")
-	GuiLayoutEnd(gui)
+  GuiLayoutBeginVertical(gui, text_x, y, true)
+  GuiText(gui, 0, 5, title, 1, "data/fonts/font_pixel_huge.xml")
+  GuiLayoutEnd(gui)
 end
 
 
@@ -65,169 +65,169 @@ local draw_respawn_ui = false
 local respawn_ui_update = nil
 
 local function CreateRespawnGui(gui, disable_cessation, on_ok, on_cancel)
-	local cessUpdateFrames = 0
-	local hovered = {}
-	local clicked = {}
-	local hover_prefix = ">"
+  local cessUpdateFrames = 0
+  local hovered = {}
+  local clicked = {}
+  local hover_prefix = ">"
 
-	return function()
-		if not draw_respawn_ui then
-			return
-		end
+  return function()
+    if not draw_respawn_ui then
+      return
+    end
 
-		local id
-		local new_id = IdFactory()
-		GuiStartFrame(gui)
+    local id
+    local new_id = IdFactory()
+    GuiStartFrame(gui)
 
-		GuiZSet(gui, -1000)
-		GuiImage(gui, new_id(), 0, 0, "data/debug/ui_background.png", 1, 1000)
-		GuiZSet(gui, -1100)
+    GuiZSet(gui, -1000)
+    GuiImage(gui, new_id(), 0, 0, "data/debug/ui_background.png", 1, 1000)
+    GuiZSet(gui, -1100)
 
-		local title_text = string.upper("Unthethered")
-		GuiDecoratedTitle(gui, new_id(), 70, title_text)
+    local title_text = string.upper("Unthethered")
+    GuiDecoratedTitle(gui, new_id(), 70, title_text)
 
-		local ok_text = "  Wake Up"
-		local cancel_text = "  Wisp Away"
-		local ok_text_w, _ = GuiGetTextDimensions(gui, ok_text)
-		local cancel_text_w, _ = GuiGetTextDimensions(gui, cancel_text)
+    local ok_text = "  Wake Up"
+    local cancel_text = "  Wisp Away"
+    local ok_text_w, _ = GuiGetTextDimensions(gui, ok_text)
+    local cancel_text_w, _ = GuiGetTextDimensions(gui, cancel_text)
 
-		local x_scale = centered_x(gui, ok_text_w + cancel_text_w + 14 + 2) -- 2 is the spacing
-		GuiLayoutBeginHorizontal(gui, x_scale, 78)
+    local x_scale = centered_x(gui, ok_text_w + cancel_text_w + 14 + 2) -- 2 is the spacing
+    GuiLayoutBeginHorizontal(gui, x_scale, 78)
 
-		id = new_id()
-		if hovered[id] then
-			GuiColorSetForNextWidget(gui, 1, 1, 0.5, 1) -- Doesn't seem to be working
-			ok_text = ok_text:gsub("^  *", hover_prefix)
-		end
+    id = new_id()
+    if hovered[id] then
+      GuiColorSetForNextWidget(gui, 1, 1, 0.5, 1) -- Doesn't seem to be working
+      ok_text = ok_text:gsub("^  *", hover_prefix)
+    end
 
-		if GuiButton(gui, id, 0, 0, ok_text) then
-			clicked[id] = true
-			disable_cessation()
-			cessUpdateFrames = cessUpdateFrames + 1
-		end
-		hovered[id] = select(3, GuiGetPreviousWidgetInfo(gui))
-		if clicked[id] and cessUpdateFrames > 1 then
-			on_ok()
-			draw_respawn_ui = false
-		end
+    if GuiButton(gui, id, 0, 0, ok_text) then
+      clicked[id] = true
+      disable_cessation()
+      cessUpdateFrames = cessUpdateFrames + 1
+    end
+    hovered[id] = select(3, GuiGetPreviousWidgetInfo(gui))
+    if clicked[id] and cessUpdateFrames > 1 then
+      on_ok()
+      draw_respawn_ui = false
+    end
 
-		id = new_id()
-		if hovered[id] then
-			GuiColorSetForNextWidget(gui, 1, 1, 0.5, 1)
-			cancel_text = cancel_text:gsub("^ *", hover_prefix)
-		end
+    id = new_id()
+    if hovered[id] then
+      GuiColorSetForNextWidget(gui, 1, 1, 0.5, 1)
+      cancel_text = cancel_text:gsub("^ *", hover_prefix)
+    end
 
-		if GuiButton(gui, id, 14, 0, cancel_text) then
-			clicked[id] = true
-			disable_cessation()
-			cessUpdateFrames = cessUpdateFrames + 1
-		end
-		hovered[id] = select(3, GuiGetPreviousWidgetInfo(gui))
-		if clicked[id] and cessUpdateFrames > 1 then
-			on_cancel()
-			draw_respawn_ui = false
-		end
+    if GuiButton(gui, id, 14, 0, cancel_text) then
+      clicked[id] = true
+      disable_cessation()
+      cessUpdateFrames = cessUpdateFrames + 1
+    end
+    hovered[id] = select(3, GuiGetPreviousWidgetInfo(gui))
+    if clicked[id] and cessUpdateFrames > 1 then
+      on_cancel()
+      draw_respawn_ui = false
+    end
 
-		GuiLayoutEnd(gui)
+    GuiLayoutEnd(gui)
 
-		if cessUpdateFrames > 0 then
-			cessUpdateFrames = cessUpdateFrames + 1
-		end
-	end
+    if cessUpdateFrames > 0 then
+      cessUpdateFrames = cessUpdateFrames + 1
+    end
+  end
 end
 
 --- You must wait a frame after running the returned clousure to de-polymorph.
 local function CessatePlayer()
-	local player_id = EntityGetWithTag("player_unit")[1]
-	if not player_id then return end
-	GetGameEffectLoadTo(player_id, "POLYMORPH_CESSATION", true)
+  local player_id = EntityGetWithTag("player_unit")[1]
+  if not player_id then return end
+  GetGameEffectLoadTo(player_id, "POLYMORPH_CESSATION", true)
 
-	local poly_player_id = EntityGetWithTag("polymorphed_cessation")[1]
-	if not poly_player_id then return end
-	-- if not assert(IsPlayer(poly_player_id)) then return end
+  local poly_player_id = EntityGetWithTag("polymorphed_cessation")[1]
+  if not poly_player_id then return end
+  -- if not assert(IsPlayer(poly_player_id)) then return end
 
-	for _, id in ipairs(assert(EntityGetAllChildren(poly_player_id))) do
-		if #EntityGetTags(id) == 0 then
-			local effect = assert(EntityGetFirstComponent(id, "GameEffectComponent"))
-			ComponentSetValue2(effect, "frames", 10 ^ 6) -- Not ideal
-			GameAddFlagRun("msg_gods_looking")
-			GameAddFlagRun("msg_gods_looking2")
+  for _, id in ipairs(assert(EntityGetAllChildren(poly_player_id))) do
+    if #EntityGetTags(id) == 0 then
+      local effect = assert(EntityGetFirstComponent(id, "GameEffectComponent"))
+      ComponentSetValue2(effect, "frames", 10 ^ 6) -- Not ideal
+      GameAddFlagRun("msg_gods_looking")
+      GameAddFlagRun("msg_gods_looking2")
 
-			return function()
-				ComponentSetValue2(effect, "frames", 1)
-				GameRemoveFlagRun("msg_gods_looking")
-				GameRemoveFlagRun("msg_gods_looking2")
-			end
-		end
-	end
+      return function()
+        ComponentSetValue2(effect, "frames", 1)
+        GameRemoveFlagRun("msg_gods_looking")
+        GameRemoveFlagRun("msg_gods_looking2")
+      end
+    end
+  end
 
-	return nil
+  return nil
 end
 
 function OnWorldPreUpdate()
-	if respawn_ui_update ~= nil and draw_respawn_ui then
-		respawn_ui_update()
-		return
-	end
+  if respawn_ui_update ~= nil and draw_respawn_ui then
+    respawn_ui_update()
+    return
+  end
 
-	local player_id = EntityGetWithTag("player_unit")[1] or EntityGetWithTag("polymorphed_player")[1]
-	if not player_id then return end
-	local damage_model = EntityGetFirstComponent(player_id, "DamageModelComponent")
-	if not damage_model then return end
+  local player_id = EntityGetWithTag("player_unit")[1] or EntityGetWithTag("polymorphed_player")[1]
+  if not player_id then return end
+  local damage_model = EntityGetFirstComponent(player_id, "DamageModelComponent")
+  if not damage_model then return end
 
-	ComponentSetValue2(damage_model, "wait_for_kill_flag_on_death", true)
-	if ComponentGetValue2(damage_model, "hp") >= ONE_HP then return end
+  ComponentSetValue2(damage_model, "wait_for_kill_flag_on_death", true)
+  if ComponentGetValue2(damage_model, "hp") >= ONE_HP then return end
 
-	local disable_cessation = CessatePlayer()
-	if not disable_cessation then return end
+  local disable_cessation = CessatePlayer()
+  if not disable_cessation then return end
 
-	respawn_ui_update = CreateRespawnGui(gui, disable_cessation,
-		function()
-			local player_id = EntityGetWithTag("player_unit")[1] or EntityGetWithTag("polymorphed_player")[1]
-			if not player_id then
-				print("failed to get player_id during respawn")
-				return
-			end
-			local damage_model = EntityGetFirstComponent(player_id, "DamageModelComponent")
-			if not damage_model then
-				print("failed to get DamageModelComponent during respawn")
-				return
-			end
+  respawn_ui_update = CreateRespawnGui(gui, disable_cessation,
+    function()
+      local player_id = EntityGetWithTag("player_unit")[1] or EntityGetWithTag("polymorphed_player")[1]
+      if not player_id then
+        print("failed to get player_id during respawn")
+        return
+      end
+      local damage_model = EntityGetFirstComponent(player_id, "DamageModelComponent")
+      if not damage_model then
+        print("failed to get DamageModelComponent during respawn")
+        return
+      end
 
-			local game_effect = GetGameEffectLoadTo(player_id, "BLINDNESS", true)
-			if game_effect then
-				ComponentSetValue2(game_effect, "frames", 120)
-			end
+      local game_effect = GetGameEffectLoadTo(player_id, "BLINDNESS", true)
+      if game_effect then
+        ComponentSetValue2(game_effect, "frames", 120)
+      end
 
-			ComponentSetValue2(damage_model, "hp",
-				ComponentGetValue2(damage_model, "max_hp"))
-			GameRegenItemActionsInPlayer(player_id)
+      ComponentSetValue2(damage_model, "hp",
+        ComponentGetValue2(damage_model, "max_hp"))
+      GameRegenItemActionsInPlayer(player_id)
 
-			if not GameHasFlagRun("ending_game_completed") then
-				EntitySetTransform(player_id, respawn_position.x, respawn_position.y)
-				EntityLoad("data/entities/misc/matter_eater.xml", respawn_position.x, respawn_position.y) -- Not sure why this exists
+      if not GameHasFlagRun("ending_game_completed") then
+        EntitySetTransform(player_id, respawn_position.x, respawn_position.y)
+        EntityLoad("data/entities/misc/matter_eater.xml", respawn_position.x, respawn_position.y) -- Not sure why this exists
 
-				for _, effect in ipairs(status_effects) do
-					EntityRemoveStainStatusEffect(player_id, effect.id)
-				end
-			else
-				ComponentSetValue2(damage_model, "kill_now", true)
-			end
-		end,
-		function()
-			local player_id = EntityGetWithTag("player_unit")[1] or EntityGetWithTag("polymorphed_player")[1]
-			if not player_id then
-				print("failed to get player_id during game over")
-				return
-			end
-			local damage_model = EntityGetFirstComponent(player_id, "DamageModelComponent")
-			if not damage_model then
-				print("failed to get DamageModelComponent during game over")
-				return
-			end
+        for _, effect in ipairs(status_effects) do
+          EntityRemoveStainStatusEffect(player_id, effect.id)
+        end
+      else
+        ComponentSetValue2(damage_model, "kill_now", true)
+      end
+    end,
+    function()
+      local player_id = EntityGetWithTag("player_unit")[1] or EntityGetWithTag("polymorphed_player")[1]
+      if not player_id then
+        print("failed to get player_id during game over")
+        return
+      end
+      local damage_model = EntityGetFirstComponent(player_id, "DamageModelComponent")
+      if not damage_model then
+        print("failed to get DamageModelComponent during game over")
+        return
+      end
 
-			ComponentSetValue2(damage_model, "kill_now", true)
-		end)
+      ComponentSetValue2(damage_model, "kill_now", true)
+    end)
 
-	draw_respawn_ui = true
+  draw_respawn_ui = true
 end
