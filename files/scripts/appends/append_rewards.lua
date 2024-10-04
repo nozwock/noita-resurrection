fe1ec262ac99e09670e06cf3885a94524d8d89cc
nozwock <57829219+nozwock@users.noreply.github.com>
2024-10-05 03:41:11 +0530

    Add a gaurd for appending to ML rewards

--- files/scripts/appends/append_rewards.lua
+++ files/scripts/appends/append_rewards.lua
@@ -1,2 +1,4 @@
-ModLuaFileAppend("mods/meta_leveling/files/for_modders/rewards_append.lua",
-  "mods/resurrection/files/scripts/appends/rewards.lua")
+if ModIsEnabled("meta_leveling") then
+  ModLuaFileAppend("mods/meta_leveling/files/for_modders/rewards_append.lua",
+    "mods/resurrection/files/scripts/appends/rewards.lua")
+end
